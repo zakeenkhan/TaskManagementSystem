24e51299c47e9cfb05a44deb6cd1c59773e58247
Zakeen khan <zakeenkhann@gmail.com>
2025-07-10 00:13:21 +0500

    non

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -5,13 +5,22 @@ const { Pool } = pkg;
 
 dotenv.config();
 
-export const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
-});
+const poolConfig = process.env.DATABASE_URL
+  ? {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }
+  : {
+      user: process.env.PG_USER,
+      host: process.env.PG_HOST,
+      database: process.env.PG_DATABASE,
+      password: process.env.PG_PASSWORD,
+      port: process.env.PG_PORT,
+    };
+
+export const pool = new Pool(poolConfig);
 
 export const connectDB = async () => {
   try {
